cf360a7b11a6aa220faf838b89905bf57710e04d
Julien Depredurand <julien.depredurand@gmail.com>
2022-09-03 22:05:07 -0700

    create assets and versions tables in separate functions

--- src/main.rs
+++ src/main.rs
@@ -45,46 +45,49 @@ impl From<&SqliteRow> for Asset {
     }
 }
 
+async fn create_assets_table(db_name: &str) -> Result<(), sqlx::Error> {
+    let mut conn = sqlite::SqliteConnection::connect(db_name).await?;
+    let ct_assets = sqlx::query(
+        r#"
+            CREATE TABLE IF NOT EXISTS "assets" (
+                "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                "name"	TEXT
+            );
+        "#,
+    )
+    .execute(&mut conn)
+    .await?;
+    Ok(())
+}
+
+async fn create_versions_table(db_name: &str) -> Result<(), sqlx::Error> {
+    let mut conn = sqlite::SqliteConnection::connect(&db_name).await?;
+    let ct_assets = sqlx::query(
+        r#"
+            CREATE TABLE IF NOT EXISTS "versions" (
+                "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+                "source"	TEXT,
+                "datapath"	TEXT,
+                "depend"	TEXT,
+                "approved"	INTEGER,
+                "status"	INTEGER,
+                "asset_id"	INTEGER,
+                FOREIGN KEY("asset_id") REFERENCES "assets"("id")
+            );
+        "#,
+    )
+    .execute(&mut conn)
+    .await?;
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     //
-    let create_assets = true;
-    let create_versions = true;
     let db_name = "sqlite:/home/bunker/assets2.db";
     //
-    if create_assets == true {
-        let mut conn = sqlite::SqliteConnection::connect(&db_name).await?;
-        let ct_assets = sqlx::query(
-            r#"
-                CREATE TABLE IF NOT EXISTS "assets" (
-                    "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                    "name"	TEXT
-                );
-            "#,
-        )
-        .execute(&mut conn)
-        .await?;
-    }
-    //
-    if create_versions == true {
-        let mut conn = sqlite::SqliteConnection::connect(&db_name).await?;
-        let ct_assets = sqlx::query(
-            r#"
-                CREATE TABLE IF NOT EXISTS "versions" (
-                    "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-                    "source"	TEXT,
-                    "datapath"	TEXT,
-                    "depend"	TEXT,
-                    "approved"	INTEGER,
-                    "status"	INTEGER,
-                    "asset_id"	INTEGER,
-                    FOREIGN KEY("asset_id") REFERENCES "assets"("id")
-                );
-            "#,
-        )
-        .execute(&mut conn)
-        .await?;
-    }
+    create_assets_table(&db_name).await?;
+    create_versions_table(&db_name).await?;
     //
     Ok(())
 }
